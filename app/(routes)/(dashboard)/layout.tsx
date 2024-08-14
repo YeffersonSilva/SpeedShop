6fa9b5d021b79e290ab9704f2462174daaa2a5f4
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-08-14 15:50:38 -0300

    chore: Add NavBarDashboard component and update dependencies

--- app/(routes)/(dashboard)/layout.tsx
+++ app/(routes)/(dashboard)/layout.tsx
@@ -1,21 +1,20 @@
-import React from 'react'
-import { Sidebar } from './dashboard/components/sidebar'
+import { NavbarDashboard } from "./dashboard/components/NavBarDashboard";
+import { Sidebar } from "./dashboard/components/sidebar";
 
-export default function LayoutDashboard
-    ({ children }: { children: React.ReactNode }) {
+export default function LayoutDashboard({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <div>
     <div className="flex w-full h-full">
       <div className="hidden h-full xl:block w-80 xl:fixed">
-        <Sidebar/>
+        <Sidebar />
       </div>
       <div className="w-full h-full xl:ml-80">
-        Navjhjhhi
-        <div className="p-6 h-max">
-          {children}
-        </div>
+        <NavbarDashboard />
+        <div className="p-6 h-max">{children}</div>
       </div>
-    </div>   
     </div>
-  )
+  );
 }
