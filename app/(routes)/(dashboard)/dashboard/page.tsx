47e5a7079bd2344e27dd696ff2f5f5a4103966e6
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-08-14 16:01:06 -0300

    chore: Update dashboard page with list of cars
    This commit updates the dashboard page to display a list of cars. It adds a new section with a title "List of cars" and adjusts the layout to accommodate the new content.

--- app/(routes)/(dashboard)/dashboard/page.tsx
+++ app/(routes)/(dashboard)/dashboard/page.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 
 export default function DashboardPage({ children }: { children: React.ReactNode }) {
   return (
-<div>Hiolalala</div>
+
+
+    <div>
+    <div className="flex justify-between">
+      <h2 className="text-2xl font-bold">List of cars</h2></div>
+    </div>
+
   );
 }
