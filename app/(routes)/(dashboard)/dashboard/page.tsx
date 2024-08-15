7090476b7c19239072ec61dcb22a6d5ceaf0046c
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-08-14 21:21:48 -0300

    feat: Add ListCars component for displaying and managing car listings
    The ListCars component and its related files have been added to the repository. This component allows for the display and management of car listings in the admin dashboard. It includes functionality for filtering, searching, sorting, and paging the listing results.

--- app/(routes)/(dashboard)/dashboard/page.tsx
+++ app/(routes)/(dashboard)/dashboard/page.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
+import { ListCars } from "./components/ListCars";
 
-export default function DashboardPage({ children }: { children: React.ReactNode }) {
+export default async function DashboardPage() {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const cars = await db.car.findMany({
+    where: {
+      isPublish: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
   return (
-
-
     <div>
-    <div className="flex justify-between">
-      <h2 className="text-2xl font-bold">List of cars</h2></div>
+      <div className="flex justify-between">
+        <h2 className="text-2xl font-bold">List of cars</h2>
+      </div>
+      <ListCars cars={cars} />
     </div>
-
   );
 }
