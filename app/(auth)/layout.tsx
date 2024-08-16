b63ae4bdfd5645b411b999e3998044470e9a414f
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-08-16 10:40:55 -0300

    feat: Update authentication page design
    This commit updates the design of the authentication page. The logo title was modified, and the Next.js Image component was added to display the TarreCars logo. A typo in the main page title was also corrected.

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
 import Image from "next/image";
 
-export default function AuthLayout
-    ({children}: {children: React.ReactNode}) {
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <div className="grid lg:grid-cols-2 h-full items-center justify-center">
       <div className="flex items-center justify-center">{children}</div>
       <div className="hidden lg:flex lg:bg-slate-300 h-full justify-center items-center lg:flex-col">
         <Image src="/logo.svg" alt="Logo TarreCars" width="80" height="80" />
-        <h1 className="text-xl font-bold">Speed Shop</h1>
+        <h1 className="text-xl font-bold">TarreCars</h1>
       </div>
     </div>
-  )
+  );
 }
