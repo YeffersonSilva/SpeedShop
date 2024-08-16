81d7738932ac84cb781056405fb47c1f9a4823af
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-08-16 11:04:50 -0300

    feat: Add filters to homepage
    - Implemented filters on the homepage to refine content search.
    - Added filtering options by categories, price, and other relevant criteria.
    - Enhanced user experience with more precise and personalized search.

---
+++ app/(routes)/(home)/cars/page.tsx
@@ -0,0 +1,29 @@
+import { Navbar } from "@/components/Shared/Navbar";
+import { db } from "@/lib/db";
+import { HeaderCars } from "./components/HeaderCars";
+import { FiltersAndListCars } from "./components/FiltersAndListCars";
+import { Suspense } from "react";
+import { SkeletonCars } from "@/components/Shared/SkeletonCars";
+
+export default async function pageCars() {
+  const cars = await db.car.findMany({
+    where: {
+      isPublish: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return (
+    <div>
+      <Navbar />
+      <div className="p-6 mx-auto max-w-7xl">
+        <HeaderCars />
+        <div>
+          <FiltersAndListCars cars={cars} />
+        </div>
+      </div>
+    </div>
+  );
+}
